d2c8aecc709664fb12407e9dfc00964d9211b089
Leyla <git@jan-leila.com>
2022-04-07 22:15:45 -0500

    moved builders to builder folder

--- src/main/java/me/jellysquid/mods/sodium/client/gui/vanilla/builders/DoubleOptionBuilder.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gui/vanilla/builders/DoubleOptionBuilder.java
@@ -1,4 +1,4 @@
-package me.jellysquid.mods.sodium.client.gui.vanilla;
+package me.jellysquid.mods.sodium.client.gui.vanilla.builders;
 
 import me.jellysquid.mods.sodium.client.gui.options.storage.OptionStorage;
 import net.minecraft.client.options.DoubleOption;
